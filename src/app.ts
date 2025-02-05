8b0b7a0fe667135cada6fb1a8c60419b9e179d94
Elian Borda <eliancamiloalejandro@gmail.com>
2025-02-05 06:30:59 -0300

    server funcionando

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express, { Application, Request, Response } from 'express';
+import Joi from 'joi';
 
 const app: Application = express();
 const port = process.env.PORT || 3000;
@@ -6,9 +7,22 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
+const schema = Joi.object({
+  message: Joi.string().required().messages({
+    'any.required': 'El campo mensaje es requerido',
+    'string.empty': 'El campo mensaje no puede estar vacío',
+    'string.base': 'El campo mensaje debe ser de tipo texto',
+  })
+})
 
-app.post('/', (req: Request, res: Response) => {
-  res.send('¡Hola TypeScript con Express!');
+app.post('/api/chat', (req, res) => {
+  // Puedes acceder a los datos enviados en el cuerpo de la solicitud
+  const { error, value } = schema.validate(req.body);
+  if (error) {
+    res.status(400).json({ error: error.message });
+  }
+  res.send(`Mensaje recibido: ${req.body.message}`);
+  
 });
 
 
--- src/bot/edges/processClimateEdge.ts
+++ src/bot/edges/processClimateEdge.ts
@@ -3,8 +3,6 @@ import { State } from "../../graph.js";
 
 export const processClimateEdge = (state: State): "process-date" | "process-numday" | typeof END => {
 
-    // console.log("a partir del tipo de mensaje procesado se determino que es: " + state.messegeType)
-
     switch (state.estimationDate){
         case "Date":
             return "process-date";
